f7a5226e41dfbf9061b1c45cdc258624df322b76
nabe1005 <taiki.w.1113@gmail.com>
2022-08-07 02:17:38 +0900

    macOS側でウィンドウサイズとタイトルバーについて調整した

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,21 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
-
-    RegisterGeneratedPlugins(registry: flutterViewController)
-
-    super.awakeFromNib()
-  }
+    override func awakeFromNib() {
+        let flutterViewController = FlutterViewController.init()
+        self.contentViewController = flutterViewController
+        
+        // タイトルバー(閉じるボタンとか最小化ボタンとかタイトルとか)の部分を非表示にする
+        // 表示したい場合はrawValueに15を指定すると全部有効
+        self.styleMask = NSWindow.StyleMask.borderless
+        // frameをセットする
+        self.animator().setFrame(self.frame, display: true, animate: true)
+        // 全ての仮想デスクトップに表示する
+        self.collectionBehavior = NSWindow.CollectionBehavior.canJoinAllSpaces
+        // 常に最前面で表示されるように
+        self.level = NSWindow.Level.init(rawValue: 2)
+        
+        RegisterGeneratedPlugins(registry: flutterViewController)
+        super.awakeFromNib()
+    }
 }
